8836e50b570a814573a47258e387f15f8e20de77
Pramod800 <timilsinapramod8888@gmail.com>
2023-02-23 07:30:52 +0545

    provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:first_app/HomePage.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 // import 'package:provider/provider.dart';
 // import 'UserProvider.dart';
 
 void main() {
+  MultiProvider(
+    providers: [],
+  );
   runApp(const MyApp());
 }
 
---
+++ lib/providers/data_provider.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class DataProvider with ChangeNotifier {
+  List<String> user = ["Pramod", "Timilsina", "ram", "Hari", "Sita"];
+
+  List<String> userList = [];
+  List<String> dyanamicUserList = [];
+  List<String> userModelLIst = [];
+
+}
